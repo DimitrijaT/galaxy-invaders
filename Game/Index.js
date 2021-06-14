7c748b75033edc88bf476ff8525f29afc572d547
Димитрија Тимески <32930367+DimitrijaT@users.noreply.github.com>
2021-06-14 11:25:54 +0200

    Update Index.js

--- Game/Index.js
+++ Game/Index.js
@@ -1060,7 +1060,7 @@
         EnemyShots = 0;
         InactiveShots = 0;
         amountOfShots = 1;
-        nukes = 9999;
+        nukes = 3;
 
         Player.x = canvas.width / 2 - 30;
         Player.y = canvas.height - 30;
